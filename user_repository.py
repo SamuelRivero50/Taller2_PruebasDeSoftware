c5acc6d785351c03a9925726f639539250e03daf
RiveroDev <samuelenriquerivero@gmail.com>
2026-04-08 09:45:53 -0500

    Use fake repository

--- user_repository.py
+++ user_repository.py
@@ -1,22 +1,20 @@
 import requests
 
+"""
 class JsonPlaceholderUserRepository:
     def get_user_email(self, user_id):
         response = requests.get(
             f'https://jsonplaceholder.typicode.com/users/{user_id}', timeout=5
         )
         response.raise_for_status()
-        return response.json()['email']
-
+        return response.json()['email']image.png
 """
+
 class JsonPlaceholderUserRepository:
     def get_user_email(self, user_id):
         raise ConnectionError("User service unavailable")
-"""
 
-"""
 class FakeUserRepository:
     def get_user_email(self, user_id):
         # TODO: retornar un email ficticio
-        _________________________________
-"""
+        return f"fakeuser{user_id}@example.com"
